03bba3ff6675bbe561ad781c7c6aff718d8e2647
ysnalpr <y.alipoor80@gmail.com>
2025-12-27 19:56:42 +0330

    Add get top crypto function to get top 10 crypto data

--- main.py
+++ main.py
@@ -24,3 +24,18 @@ def get_crypto_price(crypto):
 	except requests.exceptions.RequestException as err:
 		print("Something went wrong:", err)
 
+def get_top_cryptos():
+	try:
+		url = f"https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&api_key={api_key}"
+		response = requests.get(url)
+		response.raise_for_status()
+		data = response.json()
+		return data
+	except requests.exceptions.HTTPError as errh:
+		print("Http Error:", errh)
+	except requests.exceptions.ConnectionError as errc:
+		print("Error Connecting:", errc)
+	except requests.exceptions.Timeout as errt:
+		print("Timeout Error:", errt)
+	except requests.exceptions.RequestException as err:
+		print("Something went wrong:", err)
