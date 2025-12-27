e6b1d552b94b40ef9d62063b7d576e7a83bec863
ysnalpr <y.alipoor80@gmail.com>
2025-12-27 19:47:28 +0330

    Write the get crypto price method for getting the provided crypto price

---
+++ main.py
@@ -0,0 +1,26 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import requests
+import json, os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.getenv("API_KEY")
+
+def get_crypto_price(crypto):
+	try:
+		url = f"https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids={crypto}&api_key={api_key}"
+		response = requests.get(url)
+		response.raise_for_status()
+		data = response.json()[0]
+		return data
+	except requests.exceptions.HTTPError as errh:
+		print("Http Error:", errh)
+	except requests.exceptions.ConnectionError as errc:
+		print("Error Connecting:", errc)
+	except requests.exceptions.Timeout as errt:
+		print("Timeout Error:", errt)
+	except requests.exceptions.RequestException as err:
+		print("Something went wrong:", err)
+
